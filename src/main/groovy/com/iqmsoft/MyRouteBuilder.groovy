8fce36fc215e58c04c98f081ec75cf02c7e7a2f4
Muruga <davanon2014@gmail.com>
2021-06-21 16:46:38 +0530

    First

---
+++ src/main/groovy/com/iqmsoft/MyRouteBuilder.groovy
@@ -0,0 +1,18 @@
+package com.iqmsoft
+
+import org.apache.camel.builder.RouteBuilder
+
+/**
+ * A Camel Groovy DSL Router
+ */
+class MyRouteBuilder extends RouteBuilder {
+
+    /**
+     * Let's configure the Camel routing rules using Groovy code...
+     */
+    void configure() {
+        from("jetty:http://0.0.0.0:8080")
+          .transform { 'Today is ' + new Date() }
+    }
+
+}
